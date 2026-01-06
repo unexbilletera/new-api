80027c158e594821e9606fd816d8b87e609ad6be
gabraug <gabraug@icloud.com>
2026-01-06 16:15:33 -0300

    chore(biometric): remove comments and adjust module imports

--- src/public/biometric/biometric.module.ts
+++ src/public/biometric/biometric.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../../shared/prisma/prisma.module';
 import { JwtModule } from '../../shared/jwt/jwt.module';
 import { NotificationsModule } from '../../shared/notifications/notifications.module';
+import { SmsModule } from '../../shared/sms/sms.module';
 import { BiometricController } from './controllers/biometric.controller';
 import { BiometricService } from './services/biometric.service';
 
 @Module({
-  imports: [PrismaModule, JwtModule, NotificationsModule],
+  imports: [PrismaModule, JwtModule, NotificationsModule, SmsModule],
   controllers: [BiometricController],
   providers: [BiometricService],
   exports: [BiometricService],
