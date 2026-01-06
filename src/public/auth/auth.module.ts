fc90c05a59a132ae7ccd7dcf91aed506ab1bc103
gabraug <gabraug@icloud.com>
2026-01-06 16:14:52 -0300

    chore(auth): remove comments and adjust imports

--- src/public/auth/auth.module.ts
+++ src/public/auth/auth.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../../shared/prisma/prisma.module';
+import { SmsModule } from '../../shared/sms/sms.module';
+import { EmailModule } from '../../shared/email/email.module';
+import { NotificationsModule } from '../../shared/notifications/notifications.module';
+import { LoggerModule } from '../../shared/logger/logger.module';
+import { JwtModule } from '../../shared/jwt/jwt.module';
+import { AccessLogModule } from '../../shared/access-log/access-log.module';
+import { CronosModule } from '../../shared/cronos/cronos.module';
+import { ExchangeModule } from '../../shared/exchange/exchange.module';
+import { AuthController } from './controllers/auth.controller';
+import { AuthService } from './services/auth.service';
+
 @Module({
-  imports: [PrismaModule],
-  controllers: [],
-  providers: [],
-  exports: [],
+  imports: [
+    PrismaModule,
+    SmsModule,
+    EmailModule,
+    NotificationsModule,
+    LoggerModule,
+    JwtModule,
+    AccessLogModule,
+    CronosModule,
+    ExchangeModule,
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+  exports: [AuthService],
 })
 export class PublicAuthModule {}
 
