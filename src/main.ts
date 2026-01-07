f540686c88f046a5968ec7c907b1e0b5ed50d5aa
gabraug <gabraug@icloud.com>
2026-01-07 11:38:47 -0300

    feat: add Swagger documentation configuration

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './shared/filters/http-exception.filter';
 import { LoggingInterceptor } from './shared/interceptors/logging.interceptor';
@@ -28,7 +29,17 @@ async function bootstrap() {
 
   app.enableCors();
 
+  const config = new DocumentBuilder()
+    .setTitle('UNEX API')
+    .setDescription('UNEX API Documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000, '0.0.0.0');
   logger.info('API running on http://0.0.0.0:3000');
+  logger.info('Swagger available at http://0.0.0.0:3000/api');
 }
 bootstrap();
