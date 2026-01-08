61145553d6409d702b71515b79155fd51e5ec5d3
gabraug <gabraug@icloud.com>
2026-01-08 11:25:31 -0300

    feat: add actions-app dto file with response interfaces

---
+++ src/secure/actions-app/dto/actions-app.dto.ts
@@ -0,0 +1,57 @@
+export enum ActionSection {
+  HOME = 'home',
+  BOTTOM_TAB = 'bottom_tab',
+  MENU = 'menu',
+  QUICK_ACTION = 'quick_action',
+  SERVICES = 'services',
+}
+
+export class ListActionsQueryDto {
+  section?: ActionSection;
+  activeOnly?: boolean;
+}
+
+export interface ActionResponseDto {
+  id: string;
+  section: string;
+  type: string;
+  name: string;
+  title?: string;
+  description?: string;
+  icon?: string;
+  iconUrl?: string;
+  color?: string;
+  route?: string;
+  externalUrl?: string;
+  order: number;
+  active: boolean;
+  requiresKyc?: boolean;
+  requiresAuth?: boolean;
+  moduleKey?: string;
+}
+
+export interface ModuleResponseDto {
+  id: string;
+  key: string;
+  name: string;
+  description?: string;
+  enabled: boolean;
+}
+
+export interface ModuleStatusResponseDto {
+  moduleKey: string;
+  enabled: boolean;
+}
+
+export interface LayoutResponseDto {
+  homeActions: ActionResponseDto[];
+  bottomTabActions: ActionResponseDto[];
+  menuActions: ActionResponseDto[];
+  quickActions: ActionResponseDto[];
+  servicesActions: ActionResponseDto[];
+  modules: ModuleResponseDto[];
+}
+
+export interface ActionsWithModuleFilterResponseDto {
+  actions: ActionResponseDto[];
+}
