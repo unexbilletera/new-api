34d47f96c06b2af124ceae890956e8a06862601c
gabraug <gabraug@icloud.com>
2026-01-16 20:22:01 -0300

    feat(transactions): update module to include new controllers and services

--- src/secure/transactions/transactions.module.ts
+++ src/secure/transactions/transactions.module.ts
@@ -2,10 +2,27 @@ import { Module } from '@nestjs/common';
 import { PixCronosModule } from './pix-cronos/pix-cronos.module';
 import { PrismaModule } from '../../shared/prisma/prisma.module';
 import { TransactionalPasswordModule } from '../transactional-password/transactional-password.module';
+import { SpendingLimitsModule } from '../../shared/spending-limits/spending-limits.module';
+import { SecureExchangeModule } from '../exchange/exchange.module';
+import { TransactionsController } from './controllers/transactions.controller';
+import { TransactionsService } from './services/transactions.service';
+import { TransactionCreationController } from './controllers/transaction-creation.controller';
+import { TransactionCreationService } from './services/transaction-creation.service';
+
 @Module({
-  imports: [PrismaModule, PixCronosModule, TransactionalPasswordModule],
-  controllers: [],
-  providers: [],
-  exports: [TransactionalPasswordModule],
+  imports: [
+    PrismaModule,
+    PixCronosModule,
+    TransactionalPasswordModule,
+    SpendingLimitsModule,
+    SecureExchangeModule,
+  ],
+  controllers: [TransactionsController, TransactionCreationController],
+  providers: [TransactionsService, TransactionCreationService],
+  exports: [
+    TransactionalPasswordModule,
+    TransactionsService,
+    TransactionCreationService,
+  ],
 })
 export class SecureTransactionsModule {}
